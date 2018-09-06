4e9a1831639c757dc6a9f8a4b18d696bfa0642c0
Spomi <40448323+Spomi@users.noreply.github.com>
2018-09-06 20:22:44 +0100

    Delete config1.js

--- config1.js
+++
@@ -1,5 +0,0 @@
-Uexports.TOKEN = process.env.BOT12_TOKEN; // حط توكن البوت هنا
-
-exports.PREFIX = '1';//برفكس البوت هو يلي بيشغل لاغاني
-
-exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
